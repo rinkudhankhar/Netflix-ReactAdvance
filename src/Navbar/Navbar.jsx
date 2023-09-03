4ad761669529b5f45987dbd0a60665eeeb8ce7b3
Rinku Kumari <rkumari@L-HYD4GF6JY77-M.local>
2023-09-03 15:21:20 +0530

    first commit

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import "/Users/rkumari/Desktop/Netflix/netflix-clone/src/Navbar/Navbar.css"
+
+
+function Navbar(props) {
+    return (
+       <> <nav className='nav'>
+            <div className='nav-container'>
+                <div className='nav-logo'>
+                    <a className="navbar-brand" href="#">Netflix</a>
+                </div>
+                <ul className='nav-links'>
+                    <li><a className="navbar_otheroptions" href="#">Home</a></li>
+                    <li><a className="navbar_otheroptions" href="#">TV Shows</a></li>
+                    <li><a className="navbar_otheroptions" href="#">Movies</a></li>
+                    <li><a className="navbar_otheroptions" href="#">New & Popular</a></li>
+                    <li><a className="navbar_otheroptions" href="#">My List</a></li>
+                    <li><a className="navbar_otheroptions" href="#">Browse by Languages</a></li>
+                </ul>
+                <div >
+                    <button className='button_nav'>Search</button>
+                    <button className='button_nav'>Notifications</button>
+                    <button className='button_nav'>Profile</button>
+                </div>
+            </div>
+        </nav>
+        </>
+    
+    );
+}
+export default Navbar;
