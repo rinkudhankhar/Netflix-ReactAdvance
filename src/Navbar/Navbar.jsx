c0ba5987a5fac85cf1077921302a16e928a78367
Rinku Dhankhar <87758087+rinkudhankhar@users.noreply.github.com>
2023-09-03 20:23:30 +0530

    Update Navbar.jsx

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -23,6 +23,11 @@ function Navbar(props) {
                     <button className='button_nav'>Profile</button>
                 </div>
             </div>
+           <img
+        className="navbar__avatar"
+        src="https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png"
+        alt="User Avatar"
+      />
         </nav>
         </>
     
